db80f826e94e413a1c6452d7ea1f43b67e03e8a6
dora937nishikori <dora937@docomo.ne.jp>
2023-11-10 13:42:25 +0900

    copy

--- test_code/mecab_test.py
+++ test_code/mecab_test.py
@@ -3,11 +3,15 @@ import MeCab
 """MeCab + NEologd"""
 
 """MeCab + NEologd + 分かち書き"""
-CONTENT = 'ヒカキンには好感持てるけど'
+text = 'ヒカキンには好感持てるけど'
 #分かち書き
 #tagger = MeCab.Tagger(r'-Owakati -d "C:\mecab-ipadic-neologd"') #分かち書きと辞書の指定を同時にやるだけ
 
 #形態素解析
-tagger = MeCab.Tagger(r'-d "C:\mecab-ipadic-neologd"')
-parse = tagger.parse(CONTENT)
+m = MeCab.Tagger(r'-d "C:\mecab-ipadic-neologd"')
+nouns = [line[1] for line in m.parse(text).splitlines()
+               if "名詞" in line.split()[-1]]
+parse = m.parse(text)
+print(m)
 print(parse)
+print(nouns)
