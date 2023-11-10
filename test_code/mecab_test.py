fcbbed643c6c3d4ed4aeeb13f4347a67c140c7cd
dora937nishikori <dora937@docomo.ne.jp>
2023-11-10 16:07:51 +0900

    テストコミット

--- test_code/mecab_test.py
+++ test_code/mecab_test.py
@@ -3,15 +3,27 @@ import MeCab
 """MeCab + NEologd"""
 
 """MeCab + NEologd + 分かち書き"""
-text = 'ヒカキンには好感持てるけど'
+text = '    かわいいヒカキンには好感持てるけど'
 #分かち書き
 #tagger = MeCab.Tagger(r'-Owakati -d "C:\mecab-ipadic-neologd"') #分かち書きと辞書の指定を同時にやるだけ
 
 #形態素解析
+#インスタンス
 m = MeCab.Tagger(r'-d "C:\mecab-ipadic-neologd"')
-nouns = [line[1] for line in m.parse(text).splitlines()
-               if "名詞" in line.split()[-1]]
-parse = m.parse(text)
-print(m)
-print(parse)
-print(nouns)
+parses = m.parse(text).split('\n') #改行区切り
+morphs = [] #形態素解析
+removed = [] #空白連結
+
+for parse in parses:
+    #空白区切り
+    tmp = parse.split('\t')
+    #EOS、空白行削除
+    if len(tmp) > 1:
+        morphs.append(parse.split('\t'))
+
+#品詞抽出
+for morph in morphs:
+    if '名詞' in morph[1] or '動詞' in morph[1] or '形容詞' in morph[1]: 
+        removed.append(morph[0])
+
+print(' '.join(removed))
